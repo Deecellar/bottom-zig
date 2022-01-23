b1cfb58316ce630e355a9d00c5221036f48df583
Dan Ellis Echavarria <danellise@outlook.com>
2022-01-22 21:16:03 -0500

    Added Vectorized mem equality with zig-native-vector Build now use package instead of package paths

--- build.zig
+++ build.zig
@@ -1,14 +1,56 @@
 const std = @import("std");
-
+pub fn getAllPkg(comptime T: type) CalculatePkg(T) {
+    const info: std.builtin.TypeInfo = @typeInfo(T);
+    const declarations: []const std.builtin.TypeInfo.Declaration = info.Struct.decls;
+    var pkgs: CalculatePkg(T) = undefined;
+    var index: usize = 0;
+    inline for (declarations) |d| {
+        if (d.data == .Var) {
+            pkgs[index] = @field(T, d.name);
+            index += 1;
+        }
+    }
+    return pkgs;
+}
+fn CalculatePkg(comptime T: type) type {
+    const info: std.builtin.TypeInfo = @typeInfo(T);
+    const declarations: []const std.builtin.TypeInfo.Declaration = info.Struct.decls;
+    var count: usize = 0;
+    for (declarations) |d| {
+        if (d.data == .Var) {
+            count += 1;
+        }
+    }
+    return [count]std.build.Pkg;
+}
 pub fn build(b: *std.build.Builder) void {
+    const pkgs = struct {
+        const bottom = std.build.Pkg{
+            .name = "bottom",
+            .path = .{ .path = "bottom.zig" },
+            .dependencies = &.{vector},
+        };
+        const vector = std.build.Pkg{
+            .name = "zig-native-vector",
+            .path = .{ .path = "vendors/zig-native-vector/src/algorithms.zig" },
+        };
+
+        const args = std.build.Pkg{
+            .name = "zig-args",
+            .path = .{ .path = "vendors/zig-args/args.zig" },
+        };
+    };
+    const packages = getAllPkg(pkgs);
+
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
     const use_c: bool = b.option(bool, "use_c", "Add C as the default allocator, much faster but uses libc (defaults to false)") orelse false;
+
     var options = b.addOptions();
     options.addOption(bool, "use_c", use_c);
-    options.addOption([]const u8, "version", "v0.0.1");
+    options.addOption([]const u8, "version", "v0.0.2");
     const target = b.standardTargetOptions(.{});
 
     // Standard release options allow the person running `zig build` to select
@@ -16,8 +58,9 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("bottom-zig", "src/main.zig");
-    exe.addPackagePath("bottom", "bottom.zig");
-    exe.addPackagePath("args", "vendors/zig-args/args.zig");
+    for (packages) |p| {
+        exe.addPackage(p);
+    }
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addOptions("build_options", options);
@@ -37,12 +80,13 @@ pub fn build(b: *std.build.Builder) void {
 
     var exe_tests = b.addTest("src/main.zig");
     exe_tests.setBuildMode(mode);
-
+    exe_tests.addPackage(pkgs.bottom);
     const test_step = b.step("test-exe", "Run unit tests for the CLI App");
     test_step.dependOn(&exe_tests.step);
 
     const lib = b.addStaticLibrary("bottom-zig", "bottom.zig");
     lib.setTarget(target);
+    lib.addPackage(pkgs.vector);
     lib.setBuildMode(mode);
     lib.addOptions("build_options", options);
     if (use_c) {
@@ -52,6 +96,7 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_lib = b.addTest("bottom.zig");
     test_lib.setBuildMode(mode);
+    test_lib.addPackage(pkgs.vector);
 
     const test_lib_step = b.step("test-lib", "Run unit tests for the Library");
     test_lib_step.dependOn(&test_lib.step);
--- src/decoder.zig
+++ src/decoder.zig
@@ -1,16 +1,16 @@
 const std = @import("std");
 const bottom = @import("encoder.zig").BottomEncoder;
-
+const mem = @import("zig-native-vector");
 /// This struct is just a namespace for the decoder
 pub const BottomDecoder = struct {
-    pub fn decodeAlloc(str: [] const u8, allocator : std.mem.Allocator) ![]u8 {
+    pub fn decodeAlloc(str: []const u8, allocator: std.mem.Allocator) ![]u8 {
         var len = try std.math.divCeil(usize, str.len, bottom.max_expansion_per_byte);
-        var mem = try allocator.alloc(u8, len * 2);
-        return try decode(str,mem);
+        var memory = try allocator.alloc(u8, len * 2);
+        return try decode(str, memory);
     }
-    pub fn decode(str: [] const u8, buffer: [] u8) ![]u8 {
+    pub fn decode(str: []const u8, buffer: []u8) ![]u8 {
         var iter = std.mem.split(u8, str, "👉👈");
-        var index : usize = 0;
+        var index: usize = 0;
         while (true) {
             if (iter.next()) |owo| {
                 buffer[index] = (try decodeByte(owo));
@@ -18,38 +18,37 @@ pub const BottomDecoder = struct {
             } else {
                 break;
             }
-            
         }
-        return buffer[0..index - 1];
+        return buffer[0 .. index - 1];
     }
     pub fn decodeByte(byte: []const u8) !u8 {
         var b: u8 = 0;
         var index: usize = 0;
         while (index < byte.len) {
             if (index + 4 < byte.len + 1) {
-                if (std.mem.eql(u8, bottom.chars[0..4], byte[index .. index + 4])) {
+                if (mem.eql(u8, bottom.chars[0..4], byte[index .. index + 4])) {
                     b += 200;
                     index += 4;
                     continue;
                 }
-                if (std.mem.eql(u8, bottom.chars[4..8], byte[index .. index + 4])) {
+                if (mem.eql(u8, bottom.chars[4..8], byte[index .. index + 4])) {
                     b += 50;
                     index += 4;
                     continue;
                 }
-                if (std.mem.eql(u8, bottom.chars[8..11], byte[index .. index + 3])) {
+                if (mem.eql(u8, bottom.chars[8..11], byte[index .. index + 3])) {
                     b += 10;
                     index += 3;
                     continue;
                 }
-                if (std.mem.eql(u8, bottom.chars[11..15], byte[index .. index + 4])) {
+                if (mem.eql(u8, bottom.chars[11..15], byte[index .. index + 4])) {
                     b += 5;
                     index += 4;
                     continue;
                 }
             }
             if (byte.len > index) {
-                if (std.mem.eql(u8, ",", byte[index .. index + 1])) {
+                if (mem.eql(u8, ",", byte[index .. index + 1])) {
                     b += 1;
                     index += 1;
                     continue;
