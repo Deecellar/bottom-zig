d26661eca91acc043b911752f68a59d6449752fe
Rimuspp <19101das@gmail.com>
2026-07-06 11:30:13 -0500

    update, etoooo, bleh 2

--- src/wasm-example.zig
+++ src/wasm-example.zig
@@ -268,7 +268,7 @@ pub const std_options: std.Options = .{
 /// # Logging Function
 pub fn logFn(
     comptime message_level: std.log.Level,
-    comptime scope: @Type(.enum_literal),
+    comptime scope: @EnumLiteral(),
     comptime format: []const u8,
     args: anytype,
 ) void {
